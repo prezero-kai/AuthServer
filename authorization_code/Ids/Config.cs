8a9e1dff06390bdd8a5773f85e5f6098a43a453f
zkq <zhangkq@humanbacker.com>
2021-01-14 15:30:49 +0800

    init

---
+++ authorization_code/Ids/Config.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace Ids
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("api.weather.scope"),
+                new ApiScope("api.test.scope"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "scope1", "api.weather.scope", "api.test.scope" }
+                },
+
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:5001/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5001/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = new List<string>{
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "scope1","api.weather.scope", "api.test.scope"
+                    }
+                },
+
+                new Client
+                {
+                    ClientId = "client.password",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "scope1","api.weather.scope", "api.test.scope" }
+                },
+
+                // JavaScript Client
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+
+                    RedirectUris =           { "https://localhost:5004/callback.html" },
+                    PostLogoutRedirectUris = { "https://localhost:5004/index.html" },
+                    AllowedCorsOrigins =     { "https://localhost:5004" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "openid", "profile", "scope1", "api.weather.scope", "api.test.scope"
+                    }
+                }
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("api","#api")
+                {
+                    Scopes = { "scope1", "api.weather.scope", "api.test.scope" }
+                },
+            };
+    }
+}
