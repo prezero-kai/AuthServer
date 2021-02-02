1f7eead26c649db1466eb1c656a7ffe8fa6e4cb1
kai.zhang <zhangkq@humanbacker.com>
2021-02-02 14:33:44 +0800

    gateway consul

--- authorization_code/Ocelot.APIGateway/Startup.cs
+++ authorization_code/Ocelot.APIGateway/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace Ocelot.APIGateway
 {
@@ -22,7 +23,7 @@ namespace Ocelot.APIGateway
         {
             services.AddControllers();
             //Ìí¼Óocelot·þÎñ
-            services.AddOcelot();
+            services.AddOcelot().AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
